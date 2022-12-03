a039e7c17454e696be27f1b52a778e279a0570b2
Catarina Oliveira <catarina.oliveira003@gmail.com>
2022-12-03 16:24:44 -0300

    end points: /find-by-cpf e /total-amout-by-period

--- desafio-java/src/main/java/br/com/desafiojava/arquivo/Documentacao.java
+++ desafio-java/src/main/java/br/com/desafiojava/arquivo/Documentacao.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@Service
+
     public class Documentacao {
 
     private final LerTexto lerTexto;
@@ -20,7 +20,7 @@ import java.util.List;
         this.lerTexto = lerTexto;
     }
 
-    //caminho p/ ler texto
+    //caminho p/ ler texto - retorna tudo
     public List<Transacao> tudo() {
         String path = "C:/workspace/desafio-java";
         listaTransacao = new ArrayList<>();
@@ -29,6 +29,7 @@ import java.util.List;
         for (String linha : linhas) {
             Transacao transacao = new Transacao();
             TipoTransacao tipoTransacao = new TipoTransacao();
+
 
             //tipo de transação
             int tipo = Integer.parseInt(linha.substring(0, 1).trim());
@@ -39,7 +40,7 @@ import java.util.List;
 
             transacao.setTipoTransacao(tipoTransacao);
 
-            //data formatada
+            //data
             String data = linha.substring(1, 9).trim();
             LocalDate dataFormatada = LocalDate.parse((data));
             transacao.setData(dataFormatada);
@@ -60,7 +61,8 @@ import java.util.List;
         return listaTransacao;
     }
 
-    //all
+
+    //find-by-cpf
     public List<Transacao> procurarPorCpf (String cpf){
         List<Transacao> listaTransacaoCpf = new ArrayList<>();
         if (listaTransacao == null || listaTransacao.isEmpty()){
@@ -70,13 +72,83 @@ import java.util.List;
             if (t.getCpf().equals(cpf)) listaTransacaoCpf.add(t);
         }
         return listaTransacaoCpf;
+    }
+
+
+
+    //total-amout-by-period
+    public double entreDuasDatas (LocalDate inicio, LocalDate fim){
+
+        if (listaTransacao == null || listaTransacao.isEmpty()) {
+            tudo();
+        }
+        double periodo = 0;
+        for (Transacao t : listaTransacao){
+            if (t.getData().isBefore(fim) && t.getData().isAfter(inicio)){
+                periodo += t.getValor(); //verificar
+            }
+        }
+        return periodo;
+    }
+
+
+    //tipo de transacao
+    public String tipoTransacao (int tipo){
+        if (tipo == 1 || tipo == 4 || tipo == 5 || tipo == 6
+        || tipo == 7 || tipo == 8)
+            return "Entrada";
+
+        if (tipo == 2 || tipo == 3 || tipo == 9)
+            return "Saida";
+
+        return "";
+    }
+
+
+    //sinal da transacao
+    public String sinal (int tipo){
+        if (tipo == 1 || tipo == 4 || tipo == 5 || tipo == 6
+                || tipo == 7 || tipo == 8)
+            return "+";
+
+        if (tipo == 2 || tipo == 3 || tipo == 9)
+            return "-";
+
+        return "";
+    }
+
+
+    //descricao da transacao
+    public String descricao (int tipo) {
+        if (tipo == 1) return "Debito";
+        if (tipo == 2) return "Boleto";
+        if (tipo == 3) return "Financeiro";
+        if (tipo == 4) return "Crédito";
+        if (tipo == 5) return "Recebimento Emprestimo";
+        if (tipo == 6) return "Vendas";
+        if (tipo == 7) return "Recebimento TED";
+        if (tipo == 8) return "Recebimento DOC";
+        if (tipo == 9) return "Aluguel";
+        return "";
+    }
+
+        //data formatada
+      private static LocalDate getDataFormatada(String data){
+          String dataformat = data.substring(0,4) + "-" +
+                  data.substring(4,6) + "-" +
+                  data.substring(6, 8);
+
+          LocalDate localDate = LocalDate.parse(dataformat);
+          return LocalDate.parse(dataformat);
+
+        }
 
     }
 
 
 
 
-    }
+
 
 //    //all
 //        public Transacao findAll() throws IOException {
